545e310ce9ef396ad297766aba8979d6d9dc7b4e
KingChaka <crenshaw.brandon@yahoo.com>
2026-02-25 01:47:00 -0600

    moved files, no errors, but nothing works yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-#include "stack.h"
-#include "queue.h"
+#include <iostream>
+#include"stack.h"
+// #include "queue.h"
 
 int main(){
-	Stack toyStack;
-	Queue queueStack;
-	
+
+	Stack<int> toyStack;
 	std::cout << "printed from main" << std::endl;
+
+	//toyStack.push(5);
+	//std::cout << toyStack.top() << std::endl;
 
 	return 0;
 }
